17ad7dcf82ad7365f616ab2002788d4c4f99fd63
ItsTomBudd <thomasbudd001@gmail.com>
2025-11-06 12:17:41 +0000

    Added Frames and Pause

---
+++ GameOff2025/Assets/Scripts/S_PauseHandler.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class S_PauseHandler : MonoBehaviour
+{
+
+    [SerializeField] private GameObject pauseCanvas;
+    [SerializeField] private GameObject pauseScreen;
+    [SerializeField] private Animator animator;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100f) && hit.collider.CompareTag("PauseButton"))
+            {
+                PauseToggle();
+            }
+        }
+    }
+
+    public void PauseToggle()
+    {
+        animator.Play("ButtonPress");
+
+        if (pauseCanvas.activeSelf)
+        {
+            pauseCanvas.SetActive(false);
+            pauseScreen.SetActive(false);
+        }
+        else
+        {
+            pauseCanvas.SetActive(true);
+            pauseScreen.SetActive(true);
+        }
+    }
+}
