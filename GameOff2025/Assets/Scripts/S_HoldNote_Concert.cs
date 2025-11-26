12cf2c91f4a5fd3ab01683dab619e72fa7a7e34b
toobdani <danieltooby@yahoo.com>
2025-11-26 16:00:36 +0000

    Refined Perfect Again

--- GameOff2025/Assets/Scripts/S_HoldNote_Concert.cs
+++ GameOff2025/Assets/Scripts/S_HoldNote_Concert.cs
@@ -80,7 +80,7 @@ public class S_HoldNote_Concert : MonoBehaviour
 
             case false:
                 LerpAddition = 1 / BeatTiming;
-                EndSpawnTime = HoldLength - BeatTiming;
+                EndSpawnTime = HoldLength - 1;
                 break;
         }
         foreach (SpriteRenderer s in gameObject.GetComponentsInChildren<SpriteRenderer>())
