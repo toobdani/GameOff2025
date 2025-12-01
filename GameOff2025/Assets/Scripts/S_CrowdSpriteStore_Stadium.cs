aacc1078e611fc1cf97a4d8d63b5e6deaadfb6a8
toobdani <danieltooby@yahoo.com>
2025-12-01 17:34:29 +0000

    Current Work on Stadium

--- GameOff2025/Assets/Scripts/S_CrowdSpriteStore_Stadium.cs
+++ GameOff2025/Assets/Scripts/S_CrowdSpriteStore_Stadium.cs
@@ -24,39 +24,48 @@ public class S_CrowdSpriteStore_Stadium : MonoBehaviour
 
         switch(BeatCount)
         {
-            case 1:
+            case 0:
                 Crowds[0].HandsUp = true;
-                Crowds[1].BeginAnimation = true;
+                Crowds[0].DoneAnimation = true;
+                break;
+            case 1:
+                Crowds[1].HandsUp = true;
+                Crowds[1].DoneAnimation = true;
                 break;
             case 2:
                 Crowds[0].HandsDown = true;
-                Crowds[1].HandsUp = true;
-                Crowds[2].BeginAnimation = true;
+                Crowds[2].HandsUp = true;
+                Crowds[2].DoneAnimation = true;
+                Crowds[3].HandsUp = true;
+                Crowds[3].DoneAnimation = true;
                 break;
             case 3:
                 Crowds[1].HandsDown = true;
-                Crowds[2].HandsUp = true;
-                Crowds[3].BeginAnimation = true;
+                Crowds[4].BeginAnimation = true;
                 break;
             case 4:
                 Crowds[2].HandsDown = true;
-                Crowds[3].DoneAnimation = false;
+                Crowds[3].HandsDown = true;
+                Crowds[4].DoneAnimation = false;
                 break;
         }
         gameObject.GetComponent<S_BeatSignal_System>().BeatChanged = false;
-        BeatCount++;
+       
 
         if(BeatCount == 4)
         {
             StartedWave = false;
             BeatCount = 0;
         }
+        else BeatCount++;
     }
 
     public void StartWaves()
     {
-        Crowds[0].BeginAnimation = true;
-        BeatCount = 1;
+        Crowds[0].HandsUp = true;
+        Crowds[0].DoneAnimation = true;
+
+
         StartedWave = true;
     }
 }
